1954edb583300feff90ba0ab67f22ee65badcab4
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-07-01 23:28:27 +0100

    refactored to use mapping function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name="Beyonce"
-        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        phone="+123 456 789"
-        email="babe@beyonce.com"
+      {contacts.map(createCard)}
+
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
       />
       <Card
-        name="Jack Bauer"
-        image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        phone="+987 654 321"
-        email="jack@nowhere.com"
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
       />
       <Card
-        name="Chuck Norris"
-        image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        phone="+918 372 574"
-        email="gmail@chucknorris.com"
-      />
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
     </div>
   );
 }
